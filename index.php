5f84ff71bb81b71ab805a80a7f4bdffddbdcf634
r0b0f0sn0w <r0b0f0sn0w@users.noreply.github.com>
2019-03-16 23:59:44 -0400

    Login integrado

--- index.php
+++ index.php
@@ -1,18 +1,66 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script src="js/script.js" type="text/javascript"></script>
 	<script src="js/bootstrap.bundle.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-        <title>Docentes</title>
-    </head>
-    <body>
-        <form>
-            <input type="text" id="usuario">
-            <input type="password" id="password">
-            <button type="button" class="btn btn-success" onclick="login();">Iniciar sesión</button>
-        </form>
-    </body>
+</head>
+<body>
+
+<div class="container" style="margin-top: 90px;" >
+
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <h2>Inicio de sesión</h2>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="form-group has-danger">
+                        <label class="sr-only" for="user">Usuario</label>
+                        <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                            <div class="input-group-addon" style="width: 2.6rem"><i class="fas fa-user"></i></div>
+                            <input placeholder="Usuario" type="text" id="usuario" class="form-control user" required autofocus>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 invisible">
+                    <div class="form-control-feedback">
+                        <span class="text-danger align-middle">
+                            <i class="fa fa-close"></i> Usuario y/o contraseña incorrecto
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label class="sr-only" for="contrasenia">Contraseña</label>
+                        <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                            <div class="input-group-addon" style="width: 2.6rem"><i class="fa fa-key"></i></div>
+                            <input placeholder="Contraseña" type="password" id="password" class="form-control contrasenia" required>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+            <div class="row" style="padding-top: 1rem">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <button type="button" onclick="login();"class="btn btn-success btn-loguearse"><i class="fa fa-sign-in"></i> Entrar</button>
+                </div>
+            </div>
+<div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </html>
